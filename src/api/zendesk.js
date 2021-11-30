24e8e986649c89e98fd5f24f215ee66121743bf1
kaazii <uwaiskazi1@gmail.com>
2021-11-30 03:04:13 -0500

    Added subdomain to .env for security purposes.

--- src/api/zendesk.js
+++ src/api/zendesk.js
@@ -1,12 +1,13 @@
 const token = process.env.TOKEN
 const email = process.env.EMAIL
+const subdomain = process.env.SUBDOMAIN
 import fetch from 'node-fetch'
 console.log(fetch)
 
 const authenticator = `${email}/token:${token}`
 
 export const retrieveTickets = async function () {
-    const response = await fetch('https://zcckazi.zendesk.com/api/v2/requests.json', {
+    const response = await fetch(`https://${subdomain}.zendesk.com/api/v2/requests.json`, {
         method : 'get', 
         headers : {'Authorization': 'Basic ' + authenticator.toString('base64')}
     })
