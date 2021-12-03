d2680086fa984e4e3321db2ead1deeec392624cb
kaazii <uwaiskazi1@gmail.com>
2021-12-03 00:38:06 -0500

    Added check for negative ticket ID. Added test files

--- src/api/zendesk.js
+++ src/api/zendesk.js
@@ -2,13 +2,9 @@ import fetch from 'node-fetch'
 import { formatTicket, formatAllTickets } from '../display/formatter.js'
 
 export const retrieveTickets = async function () {
-    const token = process.env.TOKEN
-    const email = process.env.EMAIL
-    const subdomain = process.env.SUBDOMAIN
+    const authenticator = `${process.env.EMAIL}/token:${process.env.TOKEN}`
 
-    const authenticator = `${email}/token:${token}`
-
-    const response = await fetch(`https://${subdomain}.zendesk.com/api/v2/tickets.json?page=1&per_page=25`, {
+    const response = await fetch(`https://${process.env.SUBDOMAIN}.zendesk.com/api/v2/tickets.json?page=1&per_page=25`, {
         method: 'GET',
         headers: { 'Authorization': 'Basic ' + Buffer.from(authenticator).toString('base64') }
     })
@@ -24,13 +20,9 @@ export const retrieveTickets = async function () {
 }
 
 export const retrieveTicketsSpecificPage = async function (page) {
-    const token = process.env.TOKEN
-    const email = process.env.EMAIL
-    const subdomain = process.env.SUBDOMAIN
+    const authenticator = `${process.env.EMAIL}/token:${process.env.TOKEN}`
 
-    const authenticator = `${email}/token:${token}`
-
-    const response = await fetch(`https://${subdomain}.zendesk.com/api/v2/tickets.json?page=${page}&per_page=25`, {
+    const response = await fetch(`https://${process.env.SUBDOMAIN}.zendesk.com/api/v2/tickets.json?page=${page}&per_page=25`, {
         method: 'GET',
         headers: { 'Authorization': 'Basic ' + Buffer.from(authenticator).toString('base64') }
     })
@@ -46,13 +38,13 @@ export const retrieveTicketsSpecificPage = async function (page) {
 }
 
 export const retrieveSpecificTicket = async function (id) {
-    const token = process.env.TOKEN
-    const email = process.env.EMAIL
-    const subdomain = process.env.SUBDOMAIN
+    if (id < 0){
+        console.log("\n ID must be a positive number!")
+        return;
+    }
+    const authenticator = `${process.env.EMAIL}/token:${process.env.TOKEN}`
 
-    const authenticator = `${email}/token:${token}`
-
-    const response = await fetch(`https://${subdomain}.zendesk.com/api/v2/tickets/${id}.json`, {
+    const response = await fetch(`https://${process.env.SUBDOMAIN}.zendesk.com/api/v2/tickets/${id}.json`, {
         method: 'GET',
         headers: { 'Authorization': 'Basic ' + Buffer.from(authenticator).toString('base64') }
     })
